ff664b86843d96e0a13a62ad76dc06272e744051
mustafaergutt0 <mustafaergutt0@gmail.com>
2025-09-14 11:45:13 +0300

    Spring React comprehensive added First

---
+++ Airways/src/main/java/com/MERGUTholding/Airways/MergutAirwaysApplication.java
@@ -0,0 +1,13 @@
+package com.MERGUTholding.Airways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergutAirwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergutAirwaysApplication.class, args);
+	}
+
+}
